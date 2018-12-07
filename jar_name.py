3a0980e8c00c0fa013d93716bcb462c9d59a5cde
Sankarb475 <sankarb475@gmail.com>
2018-12-07 14:29:39 +0530

    Update jar_name.py

--- jar_name.py
+++ jar_name.py
@@ -1,5 +1,5 @@
 '''
-Created on 07-Dec-2018
+Created on 07-Nov-2018
 
 @author: sankar.biswas
 '''
@@ -8,13 +8,13 @@ import re
 
 def picking():
     
-    file_path = "/Users/sankar.biswas/Desktop/Noah/Random Tasks/Test Files"
+    file_path = "/Users/sankar.biswas/Desktop/Tasks/Test Files"
     files = []
     file_names = dict()
     file_names_version = dict()
     final_jar = ''
     for i in os.listdir(file_path):
-        if os.path.isfile(os.path.join(file_path,i)) and 'hermes' in i and '.jar' in i:
+        if os.path.isfile(os.path.join(file_path,i)) and 'spark_app' in i and '.jar' in i:
             file_names[i] = i[7:14]
             file_names_version[i] = re.sub('\W+','',file_names[i])
     final_jar = max(file_names_version, key = file_names_version.get)
